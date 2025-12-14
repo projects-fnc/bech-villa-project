5f9e08524a0b89eb4e07aeaf4ee01e7e7a15a784
Jestin <najstn9898@gmail.com>
2025-12-14 21:44:57 +0530

    Delete js/script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll nav
-document.querySelectorAll('.nav a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
